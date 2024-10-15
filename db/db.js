c71a6665c1624b5aae5bac96c298ba12c5496dcd
Sean Taubner <sean.taubner@gmail.com>
2024-10-15 15:18:31 -0500

    mock db created with helper functions, basic get and post routes working

--- db/db.js
+++ db/db.js
@@ -27,9 +27,52 @@ function getAllEnvelopes() {
     return envelopes
 }
 
+function addEnvelope(name, saveAmount) {
+    if (typeof name !== 'string' || typeof saveAmount !== 'number') {
+        return null;
+    }
+    if (saveAmount < 0) {
+        return -1;
+    }
+
+    const newEnvelope = {
+        name,
+        saveAmount
+    }
+
+    newEnvelope.id = crypto.randomUUID();
+    newEnvelope.spent = 0;
+    envelopes.push(newEnvelope)
+    return newEnvelope;
+}
+
+function getEnvelopeById(id) {
+    if (envelopes.length === 0) {
+        return null;
+    }
+    const foundEnvelope = envelopes.find(env => env.id === id);
+    return foundEnvelope;
+}
+
+function getEnvelopeByName(name) {
+    if (envelopes.length === 0) {
+        return null;
+    }
+    const foundEnvelope = envelopes.find(env => env.name === name);
+    return foundEnvelope;
+}
+
+function updateEnvelope(id, amountSpent, newName) {
+
+}
+
 module.exports = {
     totalBudget,
     envelopes,
     getAllEnvelopes,
+    addEnvelope,
+    getEnvelopeById,
+    getEnvelopeByName,
+    updateEnvelope,
 
 }
--- server/api.js
+++ server/api.js
@@ -4,13 +4,55 @@ const bodyParser = require('body-parser')
 
 const {
     getAllEnvelopes,
-
+    addEnvelope,
+    getEnvelopeById,
+    getEnvelopeByName,
+    updateEnvelope,
+    
 } = require('../db/db')
 
 apiRouter.use(bodyParser.json());
 
-apiRouter.get('/', (req, res) => {
+apiRouter.param('id', (req, res, next, id) => {
+    const envelope = getEnvelopeById(id);
+    if (envelope) {
+        req.envelope = envelope;
+        next();
+    } else {
+        res.status(404).send();
+    }
+})
+
+apiRouter.get('/envelopes', (req, res) => {
     res.send(getAllEnvelopes())
 });
+
+apiRouter.get('/envelopes/:id', (req, res) => {
+    const envelope = req.envelope;
+    res.status(200).send(envelope);
+})
+
+apiRouter.post('/envelopes', (req, res) => {
+    const { name, saveAmount } = req.body;
+    const dupe = getEnvelopeByName(name);
+    if (dupe) {
+        res.status(400).send("You already have an envelope with this name. Please choose a different name.")
+        return;
+    }
+
+    newEnvelope = addEnvelope(name, saveAmount);
+    if (newEnvelope === null) {
+        res.status(400).send("There's a problem with your new envelope! Please double check the name and amount entered.")
+    }
+    if (newEnvelope === -1) {
+        res.status(400).send("You've entered a negative amount for your amount to save!")
+    }
+
+    res.status(201).send(newEnvelope);
+});
+
+apiRouter.put('/envelopes/:id', (req, res) => {
+
+})
 
 module.exports = apiRouter;
